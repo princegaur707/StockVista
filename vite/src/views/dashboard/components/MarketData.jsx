8a6903d77096ed2856d7ac943713968ae893d7b2
Abhishek <abhi123923@gmail.com>
2024-11-22 20:21:48 +0530

    feat: added 30 day report

--- vite/src/views/dashboard/components/MarketData.jsx
+++ vite/src/views/dashboard/components/MarketData.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { Box, CircularProgress, Typography, CssBaseline } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import './MarketData.css';
-import { IconBackground } from '@tabler/icons-react';
 
 const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, setSymbolToken, liveMarketData }) => {
-  console.log(liveMarketData, 'bro');
-
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
-  console.log(data, 'orignal');
-
   const fetchReportData = async () => {
     try {
-      console.log('calling');
       const response = await fetch('http://localhost:8000/api/service/report/30-day-report/');
       if (!response.ok) throw new Error('Failed to fetch report data.');
 
       const result = await response.json();
-      console.log(result, 'time?');
+      const reportData = result['30_day_report'];
+
+      setData((prevData) =>
+        prevData.map((item) => {
+          const matchingReport = reportData.find((report) => report.symbol === item.tradingSymbol);
+          return matchingReport ? { ...item, price_rating: matchingReport.price_rating } : { ...item, price_rating: null }; // Assign null if no match found
+        })
+      );
     } catch (err) {
-    } finally {
-      console.log('finished calling');
+      console.error('Error fetching report data:', err);
     }
   };
 
@@ -31,16 +31,17 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
     const fetchMarketData = async () => {
       try {
         const response = await fetch('http://localhost:8000/api/service/market-data/');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+        if (!response.ok) throw new Error('Failed to fetch market data.');
+
         const jsonData = await response.json();
         setData(jsonData.data.fetched); // Assuming the fetched data is inside `data.fetched`
       } catch (err) {
         setError('Failed to fetch market data: ' + err.message);
       } finally {
         setLoading(false);
-        fetchReportData();
+        setTimeout(() => {
+          fetchReportData();
+        }, 1000);
       }
     };
 
@@ -49,15 +50,13 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
 
   useEffect(() => {
     if (liveMarketData) {
-      setData(liveMarketData);
-      console.log(liveMarketData, 'live');
-      // Find the stock in the existing data and update it
-      // setData((prevData) => prevData.map((stock) => (stock.Symbol === liveMarketData.Symbol ? { ...stock, ...liveMarketData } : stock)));
+      setData((prevData) =>
+        prevData.map((item) => (item.tradingSymbol === liveMarketData.tradingSymbol ? { ...item, ...liveMarketData } : item))
+      );
     }
   }, [liveMarketData]);
 
   const handleCellClick = (params) => {
-    // console.log('param', params.row.symbolToken);
     setSymbolToken(params.row.symbolToken);
     if (params.field === 'tradingSymbol') {
       updateToken(params.row.symbolToken);
@@ -66,7 +65,7 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
 
   if (loading) {
     return (
-      <Box style={{ alignSelf: 'center' }} display="flex" flexDirection="column" alignItems="center" height="18vh" justifyContent="center">
+      <Box display="flex" flexDirection="column" alignItems="center" height="18vh" justifyContent="center">
         <CircularProgress />
       </Box>
     );
@@ -84,7 +83,7 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
     return <Typography align="center">No Data Available</Typography>;
   }
 
-  // Filter data based on the props
+  // Filter data based on props
   let filteredData = [...data]; // Create a copy of the data
 
   if (displayTopGainers) {
@@ -94,53 +93,22 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
 
   if (displayTopLosers) {
     filteredData.sort((a, b) => a.percentChange - b.percentChange); // Sort by smallest change %
-    filteredData = filteredData.slice(0, 20); // Get bottom 20
+    filteredData = filteredData.slice(0, 20);
   }
 
   const columns = [
     { field: 'tradingSymbol', headerName: 'Symbol', headerClassName: 'header-name', flex: 1 },
+    { field: 'ltp', headerName: 'Price', headerClassName: 'header-name', flex: 1, type: 'number' },
+    { field: 'netChange', headerName: 'Change', headerClassName: 'header-name', flex: 1, type: 'number' },
+    { field: 'tradeVolume', headerName: 'Volume', headerClassName: 'header-name', flex: 1, type: 'number' },
     {
-      field: 'ltp',
-      headerName: 'Price',
-      headerClassName: 'header-name',
-      headerAlign: 'center',
-      flex: 1,
-      type: 'number',
-      renderCell: (params) => (
-        <Box display="flex" justifyContent="center" alignItems="center" height="100%">
-          <Typography align="center">{params?.value}</Typography>
-        </Box>
-      )
-    },
-
-    {
-      field: 'netChange',
-      headerName: 'Change',
+      field: 'price_rating',
+      headerName: 'Price Rating',
       headerClassName: 'header-name',
       flex: 1,
       type: 'number',
-      headerAlign: 'center',
-
-      renderCell: (params) => (
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          height="100%"
-          color={params.value > 0 ? '#00EFC8' : params.value < 0 ? '#FF5966' : '#EEEEEE'}
-        >
-          {params.value}
-        </Box>
-      )
-    },
-    {
-      field: 'tradeVolume',
-      headerName: 'Volume',
-      headerAlign: 'center',
-      headerClassName: 'header-name',
-      type: 'number',
-      flex: 1,
-      align: 'center'
+      align: 'center',
+      headerAlign: 'center'
     }
   ];
 
@@ -151,25 +119,20 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
     percentChange: row.percentChange,
     netChange: row.netChange,
     tradeVolume: row.tradeVolume,
-    buyPrice: row.depth.buy[0]?.price || 0,
-    sellPrice: row.depth.sell[0]?.price || 0,
-    buyQty: row.depth.buy[0]?.quantity || 0,
-    sellQty: row.depth.sell[0]?.quantity || 0,
+    price_rating: row.price_rating,
     symbolToken: row.symbolToken
   }));
 
   return (
-    <Box className="market-data" sx={{ mt: 0, width: ' -webkit-fill-available' }}>
-      {/* <CssBaseline /> */}
+    <Box className="market-data" sx={{ mt: 0, width: '100%' }}>
       <Box sx={{ height: '86vh' }}>
-        {/* <Box className="scrollable" style={{ height: '100vh', overflowY:'auto', marginRight: '25px'}}> */}
         <DataGrid
           rows={rows}
           columns={columns}
           pageSize={10}
-          rowsPerPageOptions={[10]} // Set the rows per page options
+          rowsPerPageOptions={[10]}
           onCellClick={handleCellClick}
-          components={{ Toolbar: GridToolbar }} // Updated for newer versions of MUI
+          components={{ Toolbar: GridToolbar }}
           pagination
           initialState={{
             pagination: {
@@ -178,9 +141,7 @@ const MarketDataTable = ({ updateToken, displayTopGainers, displayTopLosers, set
               }
             }
           }}
-          pageSizeOptions={[10]}
-          paginationMode="client" // This enables client-side pagination
-          slots={{ toolbar: GridToolbar }}
+          paginationMode="client"
         />
       </Box>
     </Box>
